9dab593bc9de487de2fe41e9c9b9a8bf4e993d8f
Jennifer Bennett <jennbbennett@hotmail.com>
2015-12-07 10:15:43 -0700

    added another method for solving problem

--- run2.js
+++ run2.js
@@ -23,4 +23,33 @@ http.createServer(function(req, res) {
     res.writeHead(200);
     res.end(servedData);
   });
-}).listen(8080);
+}).listen(8080, function(){
+  console.log('Server is listening on Port 8080');
+});
+
+// another option
+
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var port = 8080;
+
+var app = http.createServer(function(request, response){
+var parsedUrl = url.parse(request.url, true);
+fs.readFile(path.join('.', parsedUrl.pathname), 'utf-8', function(err, contents){
+  if(err){
+    respone.statusCode = 404;
+    response.end('404');
+    return;
+  } else {
+    response.statusCode = 200;
+    var regex = new RegExp(parsedUrl.query.from, 'gi')
+    response.end(contents.replace(regex, parsedUrl.query.to);)
+  }
+});
+});
+
+app.listen(8080, function(){
+  console.log('Server is listen on 8080');
+});
